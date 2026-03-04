d1420ec2498ac184edef3ce3554014e61a4b6bed
Benoit <b.danquechindorval@urios.com>
2026-03-04 11:03:32 +0100

    Updated MySQL query result filtering (deprecation messages)

--- fabtools/mysql.py
+++ fabtools/mysql.py
@@ -65,7 +65,7 @@ def query(query, use_sudo=True, filter_res=True, **kwargs):
 
 
 def _filter_res(res):
-    return re.sub(r'^(mysql: \[warning]|warning:).*\n?', '', res, flags=re.IGNORECASE)
+    return re.sub(r'^(mysql: (\[warning]|Deprecated)|warning:).*\n?', '', res, flags=re.IGNORECASE)
 
 
 def user_exists(name, host='localhost', **kwargs):
